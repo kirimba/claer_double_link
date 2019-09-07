768e5455c34d406bd4f94595a19dae0aaa28d382
NoteBook/Kirill <jeli2@yandex.ru>
2019-09-07 12:31:20 +0300

    (open check link) and write file

--- claer_double_link/Form1.Designer.cs
+++ claer_double_link/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(214, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 276);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 77);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Write file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 84);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Check file";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(528, 302);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- claer_double_link/Form1.cs
+++ claer_double_link/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace claer_double_link
 {
     public partial class Form1 : Form
     {
+        List<string> list_patch_file = new List<string>();
+        List<string> list_name_file = new List<string>();
+        List<string> list_double = new List<string>();
+        string patch = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Text|*.txt";
+            list_patch_file.Clear();
+            list_name_file.Clear();
+            list_double.Clear();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string patchfile = fileDialog.FileName;
+                MessageBox.Show(patchfile);
+                string[] line_in_file = File.ReadAllLines(patchfile);
+                string temp = "";
+                foreach(string line in line_in_file)
+                {
+                    
+                    if (list_name_file.Count > 0)
+                    {
+                        temp = line.Substring(line.LastIndexOf("/") + 1);
+                        if (!list_name_file.Contains(temp))
+                        {
+                            list_name_file.Add(temp);
+                            list_patch_file.Add(line);
+                        }
+                        else
+                        {
+                            list_double.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        temp = line.Substring(line.LastIndexOf("/") + 1);
+                        list_name_file.Add(temp);
+                        list_patch_file.Add(line);
+                    }
+                }
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(list_double.ToArray());
+                MessageBox.Show(list_double.Count.ToString());
+                patch = patchfile;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (patch != null)
+            {
+                string patchfile = patch;
+                do
+                {
+                    patchfile = patchfile.Substring(0, patchfile.LastIndexOf(".txt")) + "_new.txt";
+                } while (File.Exists(patchfile));
+                File.WriteAllLines(patchfile, list_patch_file.ToArray());
+                MessageBox.Show("OK");
+            }
         }
     }
 }
